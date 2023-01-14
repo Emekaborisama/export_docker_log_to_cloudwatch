e9e6b7209a9e4587cc3c36f596f13689481e72ad
Emeka boris ama <borisphilosophy@gmail.com>
2023-01-14 21:21:43 +0000

    frist code

---
+++ main.py
@@ -0,0 +1,29 @@
+# Python program to demonstrate
+# command line arguments
+
+
+import argparse
+import subprocess
+import os
+import sys
+
+# Initialize parser
+parser = argparse.ArgumentParser()
+
+# Adding optional argument
+parser.add_argument("-di", "--docker_image", help = "Show Output")
+parser.add_argument("-bc", "--bash_command", help = "Show Output")
+
+# Read arguments from command line
+args = parser.parse_args()
+
+print(args.docker_image)
+if args.docker_image:
+    with open("Dockerfile", "w") as f:
+        f.writelines("""FROM python:3.8 \nRUN mkdir /app \nWORKDIR /app \nADD . /app/ \nRUN pip install -r requirements.txt \nCMD ["python", "app.py"]\nEXPOSE 8000""")
+
+if args.bash_command:
+    with open("bash_script.sh", "w") as f:
+        f.writelines("""""")
+
+    # rc = subprocess.call("create_docker.sh")
